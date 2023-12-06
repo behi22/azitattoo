457d515ab392ec1dc8a177d4513f9eb39c366ecc
Behbod Babai <behibabai@gmail.com>
2023-12-06 11:24:58 -0800

    Component is now compatible with mobile screens

--- src/Components/BookingParts/Services.tsx
+++ src/Components/BookingParts/Services.tsx
@@ -6,40 +6,40 @@ const Services: React.FC<ServicesProps> = ({ services }) => {
   return (
     <div style={{ minHeight: '100vh' }}>
       <Row justify='center'>
-        <Col span={3}></Col>
-        <Col span={18}>
+        <Col span={1}></Col>
+        <Col span={22}>
           <h2>Uncover Our Variety of Services</h2>
         </Col>
-        <Col span={3}></Col>
+        <Col span={1}></Col>
       </Row>
       <Row justify='center'>
-        <Col span={3}></Col>
-        <Col span={18}>
+        <Col span={1}></Col>
+        <Col span={22}>
           <h3>
             Experience our customized precision beauty treatments designed
             exclusively for you.
           </h3>
         </Col>
-        <Col span={3}></Col>
+        <Col span={1}></Col>
       </Row>
-      <Row justify='space-around' align='top' gutter={[20, 20]}>
-        <Col span={3}></Col>
+      <Row justify='space-evenly' align='top' gutter={[30, 30]}>
         {services.map((service) => (
-          <Col flex='25%' key={service.id}>
-            <div></div>
+          <Col xs={21} md={7} key={service.id}>
             <div>
-              <a href={service.link}>
-                <img width={200} src={service.img}></img>
-              </a>
-              <h4>{service.title}</h4>
-              <p>{service.description}</p>
-              <Button type='primary' href={service.link}>
-                {service.button}
-              </Button>
+              <Col span={3}></Col>
+              <div>
+                <a href={service.link}>
+                  <img width={'75%'} src={service.img}></img>
+                </a>
+                <h4>{service.title}</h4>
+                <p>{service.description}</p>
+                <Button type='primary' href={service.link}>
+                  {service.button}
+                </Button>
+              </div>
             </div>
           </Col>
         ))}
-        <Col span={3}></Col>
       </Row>
     </div>
   );
